192ef40eacecfb74f80e1153e6a42b2574cb9dd5
DanielSalazar88 <daniel_salazar82201@elpoli.edu.co>
2022-12-09 19:28:17 -0500

    Se agregaron los precios

--- exito.py
+++ exito.py
@@ -12,12 +12,11 @@ vuelo_hotel = driver.find_element(By.XPATH, '/html/body/form/div[4]/article[1]/d
 vuelo_hotel.click()
 
 # ciudad_origen = driver.find_element(By.XPATH, '/html/body/form/div[4]/article[1]/div/div[1]/div/div[1]/div/div/div[2]/div[2]/div[3]/div[2]/div[1]/div[2]/div/div[1]/div/div[1]/div/div/input')
-# ciudad_origen.send_keys('Bogotá')
+# ciudad_origen.send_keys('Bogotá, Colombia (BOG)')
 
 ciudad_destino = driver.find_element(By.XPATH, '/html/body/form/div[4]/article[1]/div/div[1]/div/div[1]/div/div/div[2]/div[2]/div[3]/div[2]/div[1]/div[2]/div/div[1]/div/div[3]/div/div/input')
 ciudad_destino.click()
 ciudad_destino.send_keys('Punta Cana, Republica Dominicana (PUJ)')
-
 
 fecha_salida = driver.find_element(By.XPATH, '/html/body/form/div[4]/article[1]/div/div[1]/div/div[1]/div/div/div[2]/div[2]/div[3]/div[2]/div[1]/div[2]/div/div[2]/div/div[1]/div/div/input')
 fecha_salida.click()
@@ -35,13 +34,41 @@ habitacion.click()
 habitacion = driver.find_element(By.XPATH, '/html/body/form/div[4]/article[1]/div/div[1]/div/div[1]/div/div/div[2]/div[2]/div[3]/div[2]/div[1]/div[2]/div/div[4]/a')
 habitacion.click()
 
+precio1 = habitacion = driver.find_element(By.XPATH, '/html/body/div[3]/div[1]/div[2]/div[4]/div/div/div/div[2]/div[7]/div/div[1]/div[5]/div[1]/div/div/div[2]/div/div[1]/div/p[1]/span[2]').text
+precio2 = habitacion = driver.find_element(By.XPATH, '/html/body/div[3]/div[1]/div[2]/div[4]/div/div/div/div[2]/div[7]/div/div[1]/div[5]/div[2]/div/div/div[2]/div/div[1]/div/p[1]/span[2]').text
+precio3 = habitacion = driver.find_element(By.XPATH, '/html/body/div[3]/div[1]/div[2]/div[4]/div/div/div/div[2]/div[7]/div/div[1]/div[5]/div[3]/div/div/div[2]/div/div[1]/div/p[1]/span[2]').text
+precio4 = habitacion = driver.find_element(By.XPATH, '/html/body/div[3]/div[1]/div[2]/div[4]/div/div/div/div[2]/div[7]/div/div[1]/div[5]/div[4]/div/div/div[2]/div/div[1]/div/p[1]/span[2]').text
+precio5 = habitacion = driver.find_element(By.XPATH, '/html/body/div[3]/div[1]/div[2]/div[4]/div/div/div/div[2]/div[7]/div/div[1]/div[5]/div[6]/div/div/div[2]/div/div[1]/div/p[1]/span[2]').text
+precio6 = habitacion = driver.find_element(By.XPATH, '/html/body/div[3]/div[1]/div[2]/div[4]/div/div/div/div[2]/div[7]/div/div[1]/div[5]/div[6]/div/div/div[2]/div/div[1]/div/p[1]/span[2]').text
+precio7 = habitacion = driver.find_element(By.XPATH, '/html/body/div[3]/div[1]/div[2]/div[4]/div/div/div/div[2]/div[7]/div/div[1]/div[5]/div[7]/div/div/div[2]/div/div[1]/div/p[1]/span[2]').text
+precio8 = habitacion = driver.find_element(By.XPATH, '/html/body/div[3]/div[1]/div[2]/div[4]/div/div/div/div[2]/div[7]/div/div[1]/div[5]/div[8]/div/div/div[2]/div/div[1]/div/p[1]/span[2]').text
+precio9 = habitacion = driver.find_element(By.XPATH, '/html/body/div[3]/div[1]/div[2]/div[4]/div/div/div/div[2]/div[7]/div/div[1]/div[5]/div[9]/div/div/div[2]/div/div[1]/div/p[1]/span[2]').text
+precio10 = habitacion = driver.find_element(By.XPATH, '/html/body/div[3]/div[1]/div[2]/div[4]/div/div/div/div[2]/div[7]/div/div[1]/div[5]/div[10]/div/div/div[2]/div/div[1]/div/p[1]/span[2]').text
+
+
+print("----------------------------------------------------")
+print(precio1)
+print("----------------------------------------------------")
+print(precio2)
+print("----------------------------------------------------")
+print(precio3)
+print("----------------------------------------------------")
+print(precio4)
+print("----------------------------------------------------")
+print(precio5)
+print("----------------------------------------------------")
+print(precio6)
+print("----------------------------------------------------")
+print(precio7)
+print("----------------------------------------------------")
+print(precio8)
+print("----------------------------------------------------")
+print(precio9)
+print("----------------------------------------------------")
+print(precio10)
+print("----------------------------------------------------")
 
 
 
-
-
-
-
-
-time.sleep(10)
+driver.quit()
 
