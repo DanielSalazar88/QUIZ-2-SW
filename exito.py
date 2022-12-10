5ae89ba5e0c76f8f43bb0c09b32aad795604a3ef
DanielSalazar88 <daniel_salazar82201@elpoli.edu.co>
2022-12-09 19:18:20 -0500

    Automatizacion exito

---
+++ exito.py
@@ -0,0 +1,47 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+
+driver = webdriver.Chrome(executable_path= "chromedriver.exe")
+
+driver.maximize_window()
+
+driver.get("https://www.viajesexito.com/paquetes")
+
+vuelo_hotel = driver.find_element(By.XPATH, '/html/body/form/div[4]/article[1]/div/div[1]/div/div[1]/div/div/div[2]/div[2]/div[3]/div[2]/div[1]/div[1]/div/div/div/ul/li[1]/label/input')
+vuelo_hotel.click()
+
+# ciudad_origen = driver.find_element(By.XPATH, '/html/body/form/div[4]/article[1]/div/div[1]/div/div[1]/div/div/div[2]/div[2]/div[3]/div[2]/div[1]/div[2]/div/div[1]/div/div[1]/div/div/input')
+# ciudad_origen.send_keys('Bogotá')
+
+ciudad_destino = driver.find_element(By.XPATH, '/html/body/form/div[4]/article[1]/div/div[1]/div/div[1]/div/div/div[2]/div[2]/div[3]/div[2]/div[1]/div[2]/div/div[1]/div/div[3]/div/div/input')
+ciudad_destino.click()
+ciudad_destino.send_keys('Punta Cana, Republica Dominicana (PUJ)')
+
+
+fecha_salida = driver.find_element(By.XPATH, '/html/body/form/div[4]/article[1]/div/div[1]/div/div[1]/div/div/div[2]/div[2]/div[3]/div[2]/div[1]/div[2]/div/div[2]/div/div[1]/div/div/input')
+fecha_salida.click()
+fecha_salida = driver.find_element(By.XPATH, '/html/body/div[19]/div[1]/table/tbody/tr[3]/td[3]/a')
+fecha_salida.click()
+fecha_regreso= driver.find_element(By.XPATH, '/html/body/div[19]/div[1]/table/tbody/tr[4]/td[4]/a')
+fecha_regreso.click()
+
+habitacion = driver.find_element(By.XPATH, '/html/body/form/div[4]/article[1]/div/div[1]/div/div[1]/div/div/div[2]/div[2]/div[3]/div[2]/div[1]/div[2]/div/div[3]/div/div/div/div/p')
+habitacion.click()
+habitacion = driver.find_element(By.XPATH, '/html/body/form/div[4]/article[1]/div/div[1]/div/div[1]/div/div/div[2]/div[2]/div[3]/div[3]/div[1]/div[2]/div/div[3]/div/span[2]/button')
+habitacion.click()
+habitacion = driver.find_element(By.XPATH, '/html/body/form/div[4]/article[1]/div/div[1]/div/div[1]/div/div/div[2]/div[2]/div[3]/div[3]/div[2]/div[2]/button')
+habitacion.click()
+habitacion = driver.find_element(By.XPATH, '/html/body/form/div[4]/article[1]/div/div[1]/div/div[1]/div/div/div[2]/div[2]/div[3]/div[2]/div[1]/div[2]/div/div[4]/a')
+habitacion.click()
+
+
+
+
+
+
+
+
+
+time.sleep(10)
+
